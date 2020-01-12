4ebf11a1b3a39e7ee51da6e088c045f0a984876a
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-01-12 11:37:22 +0900

    完了

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-let app = require('express')();
-let http = require('http').createServer(app);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: '+msg);
+        io.emit('chat message',msg);
+    });
+    socket.on('disconnect', () => {console.log('user disconnect')})
 });
 
 http.listen(3000, () => {
